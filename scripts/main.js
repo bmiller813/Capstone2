f3c18798e71861d9b5fc20d66f292c8ae642954e
Bryan <bmiller@tb.yearup.org>
2022-10-26 15:13:02 -0400

    Trying to fix navbar, did some css changes

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ function menuBarChange(x){
 //We can work on this later
 //This is JS for the nav bar
 const navURLs = [
-    {name: "Home", url: "index.html"},
-    {name: "National Parks", url: "mountains.html"},
-    {name: "Mountains", url: "parks.html"},
+    {name: "BTG", url: "index.html"},
+    {name: "National Parks", url: "parks.html"}, 
+    {name: "Mountains", url: "mountains.html"},
 ];
 
 //helper/factory function makes a new DOM Element 
@@ -17,7 +17,7 @@ function navLink(item){
     return a;
 }
 document.addEventListener("DOMContentLoaded", () => {
-    const nav = document.getElementById("nav");
+    const nav = document.getElementById("navL");
     //innerHTML += appending strings from js template ``
     //navURLs.forEach(item => nav.innerHTML += `<a href="${item.url}">${item.name}</a>`)
     navURLs.forEach(item => nav.appendChild(navLink(item)));
