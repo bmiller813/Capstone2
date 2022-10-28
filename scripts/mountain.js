52d8e9c0d6c4f6b0f982c74e2ad5bcd4b35554dd
Bryan <bmiller@tb.yearup.org>
2022-10-28 15:00:40 -0400

    added mountain filter, started css animation for it

--- scripts/mountain.js
+++ scripts/mountain.js
@@ -13,13 +13,13 @@ function mountainTitle(item) {
 function mountainElevation(item) {
     const text = document.createElement("div");
     text.classList.add("card-subtitle");
-    text.innerHTML = item.elevation;
+    text.innerHTML = `<b>Elevation:</b><br>` + item.elevation;
     return text;
 }
 function mountainDesc(item) {
     const text = document.createElement("div");
-    text.classList.add("card-body");
-    text.innerHTML = item.desc;
+    text.classList.add("card-description");
+    text.innerHTML = `<b>Description:</b><br>`+ item.desc;
     return text;
 }
 
@@ -31,7 +31,7 @@ function mountainCard(item) {
     card.appendChild(mountainTitle(item));
     card.appendChild(mountainElevation(item));
     card.appendChild(mountainDesc(item));
-    
+
     return card;
 }
 
@@ -42,6 +42,16 @@ function selectMountain(item) {
     return option;
 }
 
+function filter() {
+    const effort = document.getElementById("effort");
+    const v = effort.selectedOptions[0].value;
+    showMountains(mountainsArray.filter(m => m.effort == v));
+}
+
+function showMountains(list) {
+    window.results.innerHTML = ""; //clear the old
+    list.forEach(m => window.results.appendChild(mountainCard(m)));
+}
 
 document.addEventListener("DOMContentLoaded", () => {
     const results = document.getElementById("results");
@@ -53,17 +63,29 @@ document.addEventListener("DOMContentLoaded", () => {
         const v = select.selectedOptions[0].value;
         const match = mountainsArray.find(m => v == m.name);
         const card = mountainCard(match)
-       // const cardImg = document.getElementsByTagName("img");
+        // const cardImg = document.getElementsByTagName("img");
         results.innerHTML = "";
         card.classList.add("big");
         card.setAttribute("id", "big");
-       // cardImg.setAttribute("id", "bigImg");
+        // cardImg.setAttribute("id", "bigImg");
         results.appendChild(card);
-        
+
     };
 
-    mountainsArray.forEach(m => results.appendChild(mountainCard(m)));
+    // mountainsArray.forEach(m => window.results.appendChild(mountainCard(m)));
+    showMountains(mountainsArray);
     select.addEventListener("change", applyFilters);
+
+
+    //HOW TO GET UNIQ LIST OF STRING FOR A SELECT FROM A LIST OF OBJECTS
+    const allEfforts = mountainsArray.map(m => m.effort);
+    const list = [...new Set(allEfforts)] //make unique using ES6 Set
+
+    //SAME AS FOR LISTS ON PARKS
+    const effort = document.getElementById("effort");
+    effort.size = list.length;
+    list.forEach(e => effort.innerHTML += `<option value"${e}">${e}</option>`);
+    effort.addEventListener("change", filter);
 });
 
 //WHOLE OTHER SECTION
